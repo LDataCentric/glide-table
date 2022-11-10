f33cb11547bf7d9ba11f4a9d2e211f53268466cd
otavioanovazzi <otavio2204@gmail.com>
2022-11-10 18:52:40 -0300

    using sparkline Extracell

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,11 +1,18 @@
 import {DataEditor,Item,GridCell, TextCell, GridCellKind, GridColumn, isEditableGridCell, CustomCell, CompactSelection, GridSelection, EditableGridCell} from "@glideapps/glide-data-grid"
-import { useExtraCells, ButtonCell, ButtonCellType } from "@glideapps/glide-data-grid-cells"
+import { useExtraCells, ButtonCell, ButtonCellType, } from "@glideapps/glide-data-grid-cells"
+import type { SparklineCell } from "@glideapps/glide-data-grid-cells/dist/ts/cells/sparkline-cell"
 import "@glideapps/glide-data-grid/dist/index.css"
 import { useCallback, useState, useMemo } from "react"
 import { TypedColumn } from "../entities/TypedColumn"
 import {generateColumns, generateFakeData} from "../fake/fakefunc"
 import { useEventListener } from "../util/util"
+import range from "lodash/range.js";
 export default function GLideTable(){
+
+  let num: number = 1;
+  function rand(): number {
+      return (num = (num * 16807) % 2147483647) / 2147483647;
+  }
 
   const [selection, setSelection] = useState<GridSelection>({
     rows: CompactSelection.empty(),
@@ -16,13 +23,32 @@ export default function GLideTable(){
   
   const columns: TypedColumn[] = useMemo(()=>generateColumns(),[])
   const getData = useCallback(([col,row]: Item): GridCell => {
-
-    return {
-      data:databaseInfo[row].content,
-      displayData:databaseInfo[row].content,
-      allowOverlay:true,
-      readonly:false,
-      kind:GridCellKind.Text
+    if(col !=1){
+      return {
+        data:databaseInfo[row].content,
+        displayData:databaseInfo[row].content,
+        allowOverlay:true,
+        readonly:false,
+        kind:GridCellKind.Text
+      }
+    }
+    else
+    {
+      num = row + 1;
+      const values = range(0, 15).map(() => rand() * 100 - 50);
+      console.log(values)
+      return {
+          kind: GridCellKind.Custom,
+          allowOverlay: false,
+          copyData: "4",
+          data: {
+              kind: "sparkline-cell",
+              values,
+              displayValues: values.map(x => Math.round(x).toString()),
+              color: row % 2 === 0 ? "#77c4c4" : "#D98466",
+              yAxis: [-50, 50],
+          },
+      } as SparklineCell;
     }
   },[databaseInfo])
 
@@ -89,7 +115,9 @@ const onCellEdited = useCallback((cell: Item, newValue: EditableGridCell) => {
     })
     return true
   }
-  return (<DataEditor onCellEdited={onCellEdited} onDelete={onDelete} onColumnResize={onColumnResize} showSearch={showSearch} getCellsForSelection={true} onSearchClose={() => setShowSearch(false)} gridSelection={selection} onColumnMoved={onColMoved} onGridSelectionChange={setSelection} getCellContent={getData} 
+  return (<DataEditor {...useExtraCells()} onCellEdited={onCellEdited} onDelete={onDelete} onColumnResize={onColumnResize} showSearch={showSearch}
+    getCellsForSelection={true} onSearchClose={() => setShowSearch(false)} gridSelection={selection} 
+    onColumnMoved={onColMoved} onGridSelectionChange={setSelection} getCellContent={getData} 
   columns={sortableResizableCols} rows={databaseInfo.length} rowMarkers="both" height={500} isDraggable={false}></DataEditor>)
 
 
